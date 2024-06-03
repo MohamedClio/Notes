751799d9f588e96aa829ba5f521500ed953b175b
@herrmclio <herrmclio@gmail.com>
2024-06-03 22:32:30 +0200

    adding some styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-
+import StickyNote2Icon from "@mui/icons-material/StickyNote2";
 function Header() {
   return (
     <header>
-      <h1>Notes</h1>
+      <h1>
+        <StickyNote2Icon />
+        Notes
+      </h1>
     </header>
   );
 }
